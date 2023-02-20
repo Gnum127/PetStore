552fff63c7afafe1eb71a9e40d492f4c352f7b2b
Gnum127 <dobrohodov127@gmail.com>
2023-02-20 16:15:56 +0300

    fix methods

--- src/test/java/steps/PetSteps.java
+++ src/test/java/steps/PetSteps.java
@@ -1,33 +1,41 @@
 package steps;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.ru.Дано;
 import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.Тогда;
+import org.junit.Assert;
 import pojo.Pet;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static pojo.Pet.petBuild;
-import static steps.CommonSteps.response;
-import static steps.CommonSteps.requestBody;
-import static steps.CommonSteps.responseBody;
-import static steps.CommonSteps.id;
+import static steps.CommonSteps.*;
 
 public class PetSteps {
 
     Pet requestPetBody;
     Pet responsePetBody;
+    ObjectMapper mapper = new ObjectMapper();
+    Pet[] pets;
 
     @Когда("^выполнен POST запрос (.*) с параметрами животного$")
     public void postPet(String link, Map<String, String> params) {
         requestPetBody = petBuild(params);
         response = given()
+                .basePath(link)
                 .body(requestPetBody)
-                .post(link);
+                .post();
         responsePetBody = response.getBody().as(Pet.class);
         id = responsePetBody.getId();
         requestPetBody.setId(responsePetBody.getId());
         responseBody = responsePetBody;
+        responsePostBody = responsePetBody;
         requestBody = requestPetBody;
+        requestPostBody = requestPetBody;
     }
 
     @Когда("^выполнен PUT запрос (.*) с параметрами животного$")
@@ -36,18 +44,53 @@ public class PetSteps {
         requestPetBody = petBuild(params);
         requestPetBody.setId(id);  // собранному телу запроса задаем актуальное значение id
         response = given()
+                .basePath(link)
                 .body(requestPetBody)
-                .put(link);
+                .put();
         responsePetBody = response.getBody().as(Pet.class);
         responseBody = responsePetBody;
         requestBody = requestPetBody;
+    }
+
+    @Когда("^выполнен PUT запрос (.*) с невалидными параметрами животного$")
+    public void putPetWrongParameters(String link, Map<String, String> params) {
+        id = responsePetBody.getId();
+        requestPetBody = petBuild(params);
+        requestPetBody.setId(id);  // собранному телу запроса задаем актуальное значение id
+        response = given()
+                .basePath(link)
+                .body(requestPetBody)
+                .put();
     }
 
     @Когда("^выполнен PUT запрос (.*) с параметрами животного, задано невалидное значение параметра id$")
     public void putPetWrongIdParameters(String link, Map<String, String> params) {
         requestPetBody = petBuild(params);
         response = given()
+                .basePath(link)
                 .body(requestPetBody)
-                .put(link);
+                .put();
+    }
+
+    @Когда("^выполнен GET запрос (.*) с параметром id животного$")
+    public void getPetId(String link) {
+        response = given()
+                .basePath(link + "/" + id)
+                .get();
+        responsePetBody = response.getBody().as(Pet.class);
+        responseBody = responsePetBody;
+    }
+
+    @Дано("^выполнен GET запрос (.*) с параметрами$")
+    public void getWithParams(String link, Map<String, String> params) throws IOException {
+        response = given()
+                .params(params)
+                .get(link);
+        pets = mapper.readValue(response.getBody().asPrettyString(), Pet[].class);
+    }
+
+    @Тогда("^тело ответа включает отправленные параметры")
+    public void responseContainsBody() {
+        Assert.assertTrue(Arrays.stream(pets).anyMatch(pet -> pet.equals(responseBody)));
     }
 }
